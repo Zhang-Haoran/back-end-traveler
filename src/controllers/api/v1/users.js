d3c561e0cae0f02a1f9bec0514e9e587b7469688
haoran zhang <haoranbackup4@gmail.com>
2021-06-16 21:46:05 +1000

    project init

---
+++ src/controllers/api/v1/users.js
@@ -0,0 +1,27 @@
+const User = require("../../../models/user");
+//PUT one user
+exports.update = async (req, res) => {};
+
+//DELETE one user
+exports.destroy = async (req, res) => {};
+
+//POST one user
+exports.store = async (req, res) => {
+  console.log(req.body);
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.status(201).send({ user });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+};
+
+//GET one user
+exports.show = async (req, res) => {};
+
+//GET all user
+exports.index = async (req, res) => {
+  return res.status(200).send("Success");
+};
---
+++ src/loaders/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const config = require("../config/app");
+module.exports = async function () {
+  const connection = await mongoose.connect(config.mongoConnection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+  return connection.connection.db;
+};
