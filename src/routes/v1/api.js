3d534545575b5e1f9d66423d0b3e183a10dee1d1
KyrieeWen <245032074@qq.com>
2021-06-21 12:28:57 +1000

    add kyrie on 21/06/2021

--- src/routes/v1/api.js
+++ src/routes/v1/api.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/newline-after-import */
 const express = require("express");
 const router = new express.Router();
 const usersControllers = require("../../controllers/api/v1/users");
