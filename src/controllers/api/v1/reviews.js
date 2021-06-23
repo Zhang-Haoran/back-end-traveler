63bdf0b150224c426d832bca9ba5c65c597c84a4
Elbert Fang <elbert.fang@megaport.com>
2021-06-23 21:12:08 +1000

    add reviews to controller

---
+++ src/controllers/api/v1/reviews.js
@@ -0,0 +1,102 @@
+const Review = require('../models/review');
+const User = require('../models/user');
+const Tour = require('../models/tour');
+const Joi = require('joi');
+
+async function getAllReviews(req, res) {
+  // db.collections.find()
+  const reviews = await Review.find().exec();
+  return res.json(reviews);
+  // Course.findById().then().catch()
+  // Course.findById((error, data) => {
+  //
+  // })
+}
+
+async function getReviewById(req, res) {
+  const { id } = req.params;
+  const review = await Review.findById(id).populate('users').exec();
+  if (!review) {
+    return res.sendStatus(404);
+  }
+  return res.json(review);
+}
+
+async function updateReviewById(req, res) {
+  const { id } = req.params;
+  const { rating, comment } = req.body;
+  const review = await Review.findByIdAndUpdate(
+    id,
+    { rating, comment },
+    { new: true }
+  );
+  if (!review) {
+    return res.sendStatus(404);
+  }
+  return res.json(review);
+}
+
+async function deleteReviewById(req, res) {
+  const { id } = req.params;
+  const review = await Review.findByIdAndDelete(id);
+  if (!review) {
+    return res.sendStatus(404);
+  }
+
+  // db.collections.updateMany()
+  await User.updateMany(
+    {
+      reviews: review._id
+    },
+    {
+      $pull: {
+        reviews: review._id
+      }
+    }
+  );
+
+  await Tour.updateMany(
+    {
+      reviews: review._id
+    },
+    {
+      $pull: {
+        reviews: review._id
+      }
+    }
+  );
+
+  return res.sendStatus(204);
+  // return res.json(course);
+}
+
+async function createReview(req, res) {
+  // const { code, name, description } = req.body;
+  // validate data
+  const numberValidator = Joi.number().min(1).max(5).required();
+  const schema = Joi.object({
+    rating: numberValidator,
+    comment: Joi.string()
+  });
+  const {rating,comment} = await schema.validateAsync(req.body, {
+    allowUnknown: true,
+    stripUnknown: true,
+    abortEarly: false
+  });
+  const existReview = await Review.findById(code).exec();
+  if (existReview) {
+    // duplicate course code
+    return res.sendStatus(409);
+  }
+  const review = new Review({ _id, rating, comment });
+  await review.save();
+  return res.status(201).json(review);
+}
+
+module.exports = {
+  getAllReviews,
+  getReviewById,
+  updateReviewById,
+  deleteReviewById,
+  createReview
+};
