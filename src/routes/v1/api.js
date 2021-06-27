04bc7809cbdca0506d9f39830f5a7064cc54ab9f
Elbert Fang <elbert.fang@megaport.com>
2021-06-27 21:23:58 +1000

    add reviews to api

--- src/routes/v1/api.js
+++ src/routes/v1/api.js
@@ -8,4 +8,11 @@ router.delete("/users/:id", usersControllers.destroy);
 router.post("/users", usersControllers.store);
 router.get("/users/:id", usersControllers.show);
 
+const reviewsControllers = require("../../controllers/api/v1/reviews");
+router.get("/reviews", reviewControllers.index);
+router.put("/reviews/:id", reviewControllers.update);
+router.delete("/reviews/:id", reviewsControllers.destroy);
+router.post("/reviews", reviewsControllers.store);
+router.get("/reviews/:id", reviewsControllers.show);
+
 module.exports = router;
