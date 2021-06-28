644db5b62afd729e0078bae9119399301c028a95
Nan Guo <guonan627@hotmail.com>
2021-06-29 09:14:21 +1000

    update by comments

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const cors = require("cors");
 
 const express = require("express");
 const loader = require("./src/loaders");
-// const { connectToDB } = require("./src/loaders/mongoose");
-// connectToDB();
 
 const app = express();
 loader.init(app);
