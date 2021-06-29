969718385af70e95badc53f786ed0da1d4f39ce5
Zhang-Haoran <haoranbackup4@gmail.com>
2021-06-29 16:38:35 +1000

    Eslint error fixed

--- src/controllers/api/v1/auth.js
+++ src/controllers/api/v1/auth.js
@@ -2,14 +2,14 @@ const { generateToken } = require('../../../utils/auth');
 const User = require('../../../models/user');
 
 // User login
-async function login(req, res) {
+exports.login = async (req, res) => {
   // Check if email or password is empty
   const isDefined = req.body.email && req.body.password;
   if (!isDefined) return res.status(400).send({ error: 'Email or password not defined' });
 
   // Check if the user is register in database
   const { email, password } = req.body;
-  const user = await User.findOne({ _id: email }).select('password');
+  const user = await User.findOne({ _id: email }).select('password').exec();
   if (!user) return res.status(404).json({ error: 'Email not found' });
 
   // Check if the password is correct
@@ -17,5 +17,4 @@ async function login(req, res) {
   if (!validatePassword) return res.status(401).json({ error: 'Invalid password' });
 
   return res.status(200).json({ token: generateToken({ email }), email });
-}
-module.exports = { login };
+};
--- src/routes/v1/api.js
+++ src/routes/v1/api.js
@@ -1,10 +1,10 @@
-/* eslint-disable import/newline-after-import */
 const express = require('express');
+
 const router = new express.Router();
 const usersControllers = require('../../controllers/api/v1/users');
 const authRoute = require('./auth');
-const auth = require('../../middleware/auth');
-router.post('/users', auth, usersControllers.store);
+
+router.post('/users', usersControllers.store);
 router.use('/auth', authRoute);
 
 module.exports = router;
