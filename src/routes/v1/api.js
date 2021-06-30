12877637d65955ab93562c1e043ee1e1395f0f9a
qinjiabo1990 <qinjiabo1990@gmail.com>
2021-06-30 22:06:09 +1000

    delete comment

--- src/routes/v1/api.js
+++ src/routes/v1/api.js
@@ -1,15 +1,8 @@
 /* eslint-disable import/newline-after-import */
 const express = require("express");
 const router = new express.Router();
-// const usersControllers = require("../../controllers/api/v1/users");
 const availabilityRoute = require("./tour/availabilities");
 const tourRoute = require("./tour/tours")
-
-// router.get("/users", usersControllers.index);
-// router.put("/users/:id", usersControllers.update);
-// router.delete("/users/:id", usersControllers.destroy);
-// router.post("/users", usersControllers.store);
-// router.get("/users/:id", usersControllers.show);
 
 router.use('/tours', tourRoute);
 router.use('/availabilities', availabilityRoute);
