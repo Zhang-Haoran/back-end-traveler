d518a9d6708cf3a9acaf65675f507b69736fabb3
liuyang <liuyangyangyang1992@gmail.com>
2021-07-01 12:45:00 +1000

    resovle conflict

--- src/models/booking.js
+++ src/models/booking.js
@@ -2,18 +2,18 @@ const mongoose = require('mongoose');
 
 // 使用schema构造器创建一个新的模式实例
 const bookingSchema = new mongoose.Schema({
-    // tour:{
-    //     type: mongoose.Schema.ObjectId,
-    //     ref: 'Tour',
-    //     required: [true, 'Booking must belong to a Tour!']
-    // },
-    // user:{
-    //     type: mongoose.Schema.ObjectId,
-    //     // type: mongoose.Schema.ObejctId,
-    //     // ref 选项告诉 Mongoose 在填充的时候使用哪个 model
-    //     ref: 'User',
-    //     required: [true, 'Booking must belong to a User!']
-    // },
+    tour:{
+        type: mongoose.Schema.ObjectId,
+        ref: 'Tour',
+        required: [true, 'Booking must belong to a Tour!']
+    },
+    user:{
+        type: mongoose.Schema.ObjectId,
+        // type: mongoose.Schema.ObejctId,
+        // ref 选项告诉 Mongoose 在填充的时候使用哪个 model
+        ref: 'User',
+        required: [true, 'Booking must belong to a User!']
+    },
     price: {
         type: Number,
         required: [true, 'Booking must have a price.'],
