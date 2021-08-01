31d4ed579861085cf8e959687ad93794b78bd774
Zhang-Haoran <haoranzhang001@gmail.com>
2021-08-02 01:41:01 +1000

    add user router

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 require("dotenv").config();
 const db = require("./utils/db");
+const userRouter = require('./routes/userRouter');
 
 const app = express();
 app.use(express.json());
+app.use('/api/v1/user', userRouter);
 
 db.connectToDB();
 module.exports = app;
--- src/routes/user.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const user = require('../controllers/userController');
-
-const router = express.Router();
-router.post('/', user.postUser);
-
-module.exports = router;
