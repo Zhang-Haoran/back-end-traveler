88d58800d66b2db9d57f1aac04a36bcdeac7346d
Zhang-Haoran <haoranzhang001@gmail.com>
2021-08-02 00:58:21 +1000

    add user model

---
+++ src/models/user.js
@@ -0,0 +1,22 @@
+const {Schema, model} = require("mongoose");
+
+const userSchema = new Schema({
+    _id:{
+        type: String,
+        alias:"email"
+    },
+    firstName: {
+        type: String,
+    },
+    lastName:{
+        type: String,
+    },
+    dateOfBirth:{
+        type: String,
+    },
+    password:{
+        type: String,
+    },
+})
+
+module.exports = model("User", userSchema);
