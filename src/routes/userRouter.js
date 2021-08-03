97332cee228840c6c490a10832553510db8dcc83
Zhang-Haoran <haoranzhang001@gmail.com>
2021-08-03 17:31:42 +1000

    add comment

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,10 +2,15 @@ const express = require('express');
 const userController = require('../controllers/userController');
 
 const router = express.Router();
+// Create user
 router.post('/', userController.postUser);
+// Get all users
 router.get('/', userController.getUsers);
+// Get user by email id
 router.get('/:id', userController.getUserById);
+// Update user by email id
 router.patch('/:id', userController.updateUser);
+// Delete user by email id
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
