d8ad7425cc1fe450216bf225864e5985c7e66168
Zhang-Haoran <haoranzhang001@gmail.com>
2021-08-04 17:27:09 +1000

    add tourController

--- src/controllers/tourController.js
+++ src/controllers/tourController.js
@@ -19,4 +19,27 @@ exports.postTour = async (req, res) => {
     endDate = null,
     map = null,
   } = req.body;
+  const tour = new Tour({
+    title,
+    subtitle,
+    city,
+    state,
+    map,
+    introduction,
+    highlights,
+    included,
+    itinerary,
+    price,
+    image,
+    startDate,
+    endDate,
+  });
+  if (moment(startDate, 'DD/MM/YYYY').isAfter(moment(endDate, 'DD/MM/YYYY')))
+    return res.status(400).send('End date should not be prior to the start date');
+  try {
+    await tour.save();
+    res.status(201).json(tour);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
